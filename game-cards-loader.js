74100841fb61031863d7f3d3b487133d6b00d3f6
WolcenOn <fhaldon@gmail.com>
2026-09-17 18:29:15 +0100

    Load texture and decoration modules

--- game-cards-loader.js
+++ game-cards-loader.js
@@ -5,10 +5,13 @@
     "game-cards-layouts.js",
     "game-cards-edges.js",
     "game-cards-sizing.js",
+    "game-cards-decoration.js",
     "game-cards-render.js",
+    "game-cards-decoration-render.js",
     "game-cards-ui.js",
     "game-cards-size-ui.js",
     "game-cards-print-ui.js",
+    "game-cards-decoration-ui.js",
     "game-cards-ui-enhance.js"
   ];
   let index = 0;
